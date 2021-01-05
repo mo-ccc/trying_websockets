19ee3b1acac673249b51aed3cb477635f78d58e7
Mo <css012002@coderacademy.edu.au>
2021-01-05 19:10:32 +1100

    small html page with javascript to use with python websocket

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
-import asyncion
 import websockets
+import asyncio
+
+async def main(websocket, path):
+    while True:
+        msg = await websocket.recv()
+        print(msg)
+    
+server = websockets.serve(main, "localhost", 8080)
+asyncio.get_event_loop().run_until_complete(server)
+asyncio.get_event_loop().run_forever()
