f729171d63339fabbeffa6d92f71a1b6ed519e7c
Mo <css012002@coderacademy.edu.au>
2021-01-06 16:32:27 +1100

    starting out

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 import websockets
 import asyncio
+import os
+import json
 
 async def main(websocket, path):
+    if not os.path.exists("messages"):
+        f = open("messages", "x")
+        f.close()
+    with open("messages", 'r') as f:
+        await websocket.send(f.read())
     while True:
         msg = await websocket.recv()
-        print(msg)
+        with open("messages", 'a') as f:
+            f.write(json.dumps({"id": 1, "content": msg}))
     
 server = websockets.serve(main, "localhost", 8080)
 asyncio.get_event_loop().run_until_complete(server)
