98d58803155feb5d967792928c9f102bd2ab438b
EC2 Default User <ec2-user@ip-172-31-2-80.ec2.internal>
2021-01-06 07:02:53 +0000

    implementing dynamodb support

--- src/main.py
+++ src/main.py
@@ -2,18 +2,32 @@ import websockets
 import asyncio
 import os
 import json
+import boto3
+import boto3.dynamodb.conditions as dynamodb
+import sys
+import uuid
+from datetime import datetime
+
+client = boto3.resource('dynamodb')
+table = client.Table('websockets_messaging_app')
+if not table:
+    print("table not found")
+    sys.exit(1)
 
 async def main(websocket, path):
-    if not os.path.exists("messages"):
-        f = open("messages", "x")
-        f.close()
-    with open("messages", 'r') as f:
-        await websocket.send(f.read())
+    print("user connected")
+    old_messages = table.scan(Limit=10)["Items"]
+    await websocket.send(json.dumps(old_messages))
     while True:
         msg = await websocket.recv()
-        with open("messages", 'a') as f:
-            f.write(json.dumps({"id": 1, "content": msg}))
+        new_item = {"message_id": str(uuid.uuid4()), "content": msg, "timestamp": str(datetime.utcnow())}
+        table.put_item(
+            Item = new_item
+        )
+        await websocket.send(json.dumps(new_item))
+
     
-server = websockets.serve(main, "localhost", 8080)
+server = websockets.serve(main, "0.0.0.0", 8080)
+print("server")
 asyncio.get_event_loop().run_until_complete(server)
 asyncio.get_event_loop().run_forever()
